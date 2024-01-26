3c76c3c60bcfa33b07a6dd2d0c430447e3b9b493
dm22dh <dm22dh@brocku.ca>
2024-01-25 21:28:33 -0500

    created animated sphere

--- week_03/script.js
+++ week_03/script.js
@@ -9,6 +9,7 @@ const canvas = document.querySelector('.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+scene.background = new THREE.Color('gray')
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -17,13 +18,28 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     100
 )
+camera.position.set(0, 0, 5)
 
 scene.add(camera)
+
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+/* MESHES */
+
+// testsphere
+const sphereGeometry = new THREE.SphereGeometry(1)
+const sphereMaterial = new THREE.MeshNormalMaterial()
+const testsphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+
+scene.add(testsphere)
+
 
 
 /* Animation Loop */
@@ -36,6 +52,9 @@ const animation = () =>{
     // return elapsed time
     const elapsedTime = clock.getElapsedTime()
 
+    // Animate testsphere
+    testsphere.position.z = Math.sin(elapsedTime)
+
 
     // renderer
     renderer.render(scene, camera)
@@ -43,3 +62,5 @@ const animation = () =>{
     // request next frame
     window.requestAnimationFrame(animation)
 }
+
+animation()
